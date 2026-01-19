4c58689c79c851db132a5e130e408d5904bdfc70
VigneshMTarkasha <vignesh.m@tarkasha.com>
2026-01-19 11:36:59 +0530

    check 2

--- arcus-ui/src/App.tsx
+++ arcus-ui/src/App.tsx
@@ -6,9 +6,7 @@ import './App.css'
 function App() {
   return (
     <>
- <div className="bg-blue-500">Hello</div>
- <div className="bg-blue-500">Hello</div>
- <div className="">Hello World</div>
+      <div className="bg-blue-500">Hello</div>
     </>
   )
 }
