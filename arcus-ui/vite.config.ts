00a1c73747bfa020c6616a547a24452072f45d05
Aarthy <aarthy1012@outlook.com>
2026-01-23 10:55:34 +0530

    Update vite.config.ts

--- arcus-ui/vite.config.ts
+++ arcus-ui/vite.config.ts
@@ -2,40 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
   server: {
-    //    proxy: {
-    //   "/api": {
-    //     target: "https://contractional-napoleon-superblessed.ngrok-free.dev/api/v1",
-    //     secure: false,
-    //     changeOrigin: true,
-    //     ws:true,
-    //     rewrite: path => path.replace(/^\/api/, '')
-    //   }
-    // },
-    //  proxy: {
-    //   "/api": {
-    //     target: "https://contractional-napoleon-superblessed.ngrok-free.dev",
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    // },
      proxy: {
-      "/api": {  // Proxy all /api/* requests
-        target: "https://contractional-napoleon-superblessed.ngrok-free.dev",
+      "/api": { 
+        target: "https://robbi-ungouty-babette.ngrok-free.dev",
         changeOrigin: true,
         secure: false,
       },},
-    //  proxy: {
-    //   "/api": {
-    //     target: "http://localhost:3000",  // Replace 3000 with your backend's port
-    //     changeOrigin: true,
-    //   },
-    // },
     open: true,
     port: 5173,
     host: "0.0.0.0"
