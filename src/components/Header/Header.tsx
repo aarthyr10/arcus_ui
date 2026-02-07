3849f4d2a75c6582969b9df7708ed51b3b8522e5
vijay-tarkasha <vijay.g@tarkasha.com>
2026-02-07 15:17:07 +0530

    final fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
     <header
       className="
       fixed top-0 left-0 w-full z-50
-      backdrop-blur-md h-[11vh] md:h-[8vh] md:h-[11vh] lg:h-[10vh]
+      backdrop-blur-md h-[11vh] md:h-[9vh] lg:h-[80px]
     "
     >
       <div className="w-full mx-auto flex justify-between items-center px-4 py-4 mt-2 sm:mt-1">
